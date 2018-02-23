02310d426e6a0aacc5a6c929871a16f457379228
demonli <demonli@tvunetworks.com>
2018-02-23 13:54:56 +0800

    Initial Commit

---
+++ XDXScreenOrientationChangedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XDXScreenOrientationChangedDemo
+//
+//  Created by 小东邪 on 23/02/2018.
+//  Copyright © 2018 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XDXScreenOrientationChangedDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XDXScreenOrientationChangedDemo
+//
+//  Created by 小东邪 on 23/02/2018.
+//  Copyright © 2018 小东邪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
